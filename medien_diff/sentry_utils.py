c3f503df81eab94d7298970743231ebc2daf25c7
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2020-07-26 17:03:42 +0200

    attach full response content, always

--- medien_diff/sentry_utils.py
+++ medien_diff/sentry_utils.py
@@ -5,3 +5,4 @@ def tag_http_response(response):
     with sentry_sdk.configure_scope() as scope:
         scope.set_tag("status", response.status_code)
         scope.fingerprint = ["{{ default }}", response.status_code]
+        scope.set_extra("response_content", response.content)
